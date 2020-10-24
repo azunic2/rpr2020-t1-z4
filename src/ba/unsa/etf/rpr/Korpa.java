2c2a5f0c640f046270f522872e9be0669d82b123
Harun Skender <harun.skender@hotmail.com>
2020-10-24 23:48:08 +0200

    Project almost done.

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new  Artikl[50];
+    private static int brojac = 0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if(brojac == 50) return false;
+        artikli[brojac++] = a;
+
+        return true;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < artikli.length; i++) {
+            if(artikli[i].getKod().equals(kod)) {
+                //Ovdje ide algo za izbacivanje to be written
+                brojac--;
+                Artikl returnedArtikal = artikli[i];
+                int j;
+                for(j = i; j < artikli.length-1; j++) artikli[j] = artikli[j + 1];
+                artikli[j] = null;
+                return returnedArtikal;
+            }
+        }
+        return null;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for(int i = 0; !(artikli[i] == null); i++) suma+=artikli[i].getCijena();
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    public static int brojac = 0;
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+    public boolean dodajArtikl(Artikl a) {
+        if(brojac == 1000) return false;
+        artikli[brojac] = a;
+        brojac++;
+        return true;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+    Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < artikli.length; i++) {
+            if(artikli[i].getKod().equals(kod)) {
+                //Ovdje ide algo za izbacivanje to be written
+                brojac--;
+                Artikl returnedArtikal = artikli[i];
+                int j;
+                for(j = i; j < artikli.length-1; j++) artikli[j] = artikli[j + 1];
+                artikli[j] = null;
+                return returnedArtikal;
+            }
+        }
+        return null;
+    }
 }
